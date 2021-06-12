e7027421928c3cd353395822477a71d1d05e33d0
HoangHieu11111999 <hoanghieu11111999@gmail.com>
2021-06-12 09:37:40 +0700

    update change click

--- colabproject/src/views/problemthree.vue
+++ colabproject/src/views/problemthree.vue
@@ -1,128 +1,181 @@
 <template>
-    <div class="mt-1">
-        <b-row>
-         <div class="text-left"><b-btn @click="$router.push('/salesgrid')" variant="transparent"><font-awesome-icon size="2x" :icon="['fas', 'arrow-left']" /></b-btn></div>
-        </b-row>
-         <b-card no-body>
-    <b-tabs card align="center">
-      <b-tab title="Payment" active>
-         <b-row>
-        <b-col cols="6">
-              <b-list-group horizontal="md" class="row rounded flex-fill ">
-            <b-list-group-item button class="col-5 rounded border mr-2 shadow" variant="info" cols="1">
-              <div class="text-center"><b>SHOW TENDERS</b></div>
-                          </b-list-group-item>
+  <div class="mt-1">
+    <b-row>
+      <div class="text-left">
+        <b-btn @click="$router.push('/salesgrid')" variant="transparent"
+          ><font-awesome-icon size="2x" :icon="['fas', 'arrow-left']"
+        /></b-btn>
+      </div>
+    </b-row>
+    <b-card no-body>
+      <b-tabs card align="center">
+        <b-tab title="Payment" active>
+          <b-row>
+            <b-col cols="6">
+              <b-list-group horizontal="md" class="row rounded flex-fill">
+                <b-list-group-item
+                  button
+                  class="col-5 rounded border mr-2 shadow"
+                  variant="info"
+                  cols="1"
+                >
+                  <div class="text-center"><b>SHOW TENDERS</b></div>
+                </b-list-group-item>
               </b-list-group>
-        </b-col>
-        <b-col class="text-right">
-                         <b-list-group horizontal="md" class="row rounded flex-fill ">
-            <b-list-group-item button class="col-5 rounded border mr-2 shadow"  variant="info" cols="1">
-              <div class="text-center"><b>SPLIT BY COVERS</b></div>
-                          </b-list-group-item>
-                           <b-list-group-item button class="col-5 rounded border mr-2 shadow" variant="info" cols="1">
-              <div class="text-center"><b>SPLIT BY ITEM</b></div>
-                          </b-list-group-item>
+            </b-col>
+            <b-col class="text-right">
+              <b-list-group horizontal="md" class="row rounded flex-fill">
+                <b-list-group-item
+                  button
+                  class="col-5 rounded border mr-2 shadow"
+                  variant="info"
+                  cols="1"
+                >
+                  <div class="text-center"><b>SPLIT BY COVERS</b></div>
+                </b-list-group-item>
+                <b-list-group-item
+                  button
+                  class="col-5 rounded border mr-2 shadow"
+                  variant="info"
+                  cols="1"
+                >
+                  <div class="text-center"><b>SPLIT BY ITEM</b></div>
+                </b-list-group-item>
               </b-list-group>
-        </b-col>
-            </b-row>
-            <b-row class="mt-4 mb-4">
-                <b-col class="bg-light border-right shadow"  cols="10" md="4">
-                    <div class="mt-2">AMOUNT DUE</div>
-                    <div ><h3>0.00</h3></div>
-                    <div class="mt-3">BASKET DISCOUNT</div>
-                    <div ><h4>0.00</h4></div>
-                    <div class="mt-3">CUSTOMER DISCOUNT</div>
-                    <div ><h4>0.00</h4></div>
-                    <div class="mt-3">CUSTOMER POINT SPEND</div>
-                    <div><h4>0.00</h4></div>
-                     </b-col>
-                       <b-col>
-                           <b-row>
-                               <b-col style="font-size: 2.25rem;" class="bg-light border shadow text-right ml-3 mb-3" md="11" >
-                                   0.00
-                               </b-col>
-                           </b-row>
-                               <b-row
-     v-for="(row, n) in list"
-    :key="`row${n}`"
-    class="text-center center row container-fluid mb-2"
-
-  >
-    <b-list-group horizontal="md" class="row px-md-6 flex-fill ml-1" >
-      <b-list-group-item
-        v-for="(item, key) in row"
-        :key="`item${key}`"
-        button
-        class="col mr-1 ml-1 rounded border shadow"
-        value={value}
-                              >
-        <template v-if="item.paynum">
-          <div class="text-center"><h4>{{ item.paynum }}</h4></div>
-                       </template>
-        <span v-else> {{ item }} </span>
-      </b-list-group-item>
-    </b-list-group>
-  </b-row>
+            </b-col>
+          </b-row>
+          <b-row class="mt-4 mb-4">
+            <b-col class="bg-light border-right shadow" cols="10" md="4">
+              <div class="mt-2">AMOUNT DUE</div>
+              <div><h3>0.00</h3></div>
+              <div class="mt-3">BASKET DISCOUNT</div>
+              <div><h4>0.00</h4></div>
+              <div class="mt-3">CUSTOMER DISCOUNT</div>
+              <div><h4>0.00</h4></div>
+              <div class="mt-3">CUSTOMER POINT SPEND</div>
+              <div><h4>0.00</h4></div>
+            </b-col>
+            <b-col>
+              <b-row>
+                <b-col
+                  style="font-size: 2.25rem"
+                  class="bg-light border shadow text-right ml-3 mb-3"
+                  md="11"
+                >
+                  {{ numFormatted }}
                 </b-col>
-            </b-row>
-                <b-row>
-                <b-col cols="8">
-                         <b-list-group horizontal="md" class="row rounded flex-fill ">
-            <b-list-group-item button class="col-5 rounded border mr-2 shadow" variant="info" cols="1">
-              <div class="text-center"><b>POINTS [0/0.00]</b></div>
-                          </b-list-group-item>
+              </b-row>
+              <b-row
+                v-for="(row, n) in list"
+                :key="`row${n}`"
+                class="text-center center row container-fluid mb-2"
+              >
+                <b-list-group
+                  horizontal="md"
+                  class="row px-md-6 flex-fill ml-1"
+                >
+                  <b-list-group-item
+                    v-for="(item, key) in row"
+                    :key="`item${key}`"
+                    button
+                    class="col mr-1 ml-1 rounded border shadow"
+                    value="{value}"
+                  >
+                    <template v-if="item.paynum">
+                      <div class="text-center">
+                        <h4 @click="formatNum(item)">
+                          {{ item.paynum }}
+                        </h4>
+                      </div>
+                    </template>
+                    <span v-else> {{ item }} </span>
+                  </b-list-group-item>
+                </b-list-group>
+              </b-row>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="8">
+              <b-list-group horizontal="md" class="row rounded flex-fill">
+                <b-list-group-item
+                  button
+                  class="col-5 rounded border mr-2 shadow"
+                  variant="info"
+                  cols="1"
+                >
+                  <div class="text-center"><b>POINTS [0/0.00]</b></div>
+                </b-list-group-item>
 
-              <b-list-group-item button class="col-3 rounded border mr-2 shadow" variant="info" cols="1">
-              <div class="text-center"><b>CASH</b></div>
-                          </b-list-group-item>
+                <b-list-group-item
+                  button
+                  class="col-3 rounded border mr-2 shadow"
+                  variant="info"
+                  cols="1"
+                >
+                  <div class="text-center"><b>CASH</b></div>
+                </b-list-group-item>
 
-              <b-list-group-item button class="col-3 rounded border mr-2 shadow" variant="info" cols="1">
-              <div class="text-center"><b>CARD</b></div>
-                          </b-list-group-item>
+                <b-list-group-item
+                  button
+                  class="col-3 rounded border mr-2 shadow"
+                  variant="info"
+                  cols="1"
+                >
+                  <div class="text-center"><b>CARD</b></div>
+                </b-list-group-item>
               </b-list-group>
-                </b-col>
-                       <b-col>
-                </b-col>
-            </b-row>
-
-              </b-tab>
-      <b-tab title="Discount">
-        <b-card-text>Feature Not Yet Available</b-card-text>
-      </b-tab>
-    </b-tabs>
-  </b-card>
-</div>
+            </b-col>
+            <b-col> </b-col>
+          </b-row>
+        </b-tab>
+        <b-tab title="Discount">
+          <b-card-text>Feature Not Yet Available</b-card-text>
+        </b-tab>
+      </b-tabs>
+    </b-card>
+  </div>
 </template>
 <script>
 export default {
-  name: 'SalesGridPay',
+  name: "SalesGridPay",
+  methods: {
+    formatNum(data) {
+      let string = ``;
+      if (data.type == "number") {
+        string = `0.0${data.paynum}`;
+      } else string = data.paynum;
+      return (this.numFormatted = string);
+    },
+  },
+  created() {},
   data: () => ({
     list: [
       [
-        { paynum: '1' },
-        { paynum: '2' },
-        { paynum: '3' },
-        { paynum: '$5' }
-
-      ], [
-        { paynum: '4' },
-        { paynum: '5' },
-        { paynum: '6' },
-        { paynum: '$10' }
-      ], [
-        { paynum: '7' },
-        { paynum: '8' },
-        { paynum: '9' },
-        { paynum: '$20' }
-
-      ], [
-        { paynum: '.' },
-        { paynum: '0' },
-        { paynum: 'X', inconz: 'th' },
-        { paynum: '$55' }
-
-      ]
-    ]
-  })
+        { paynum: "1", type: "number" },
+        { paynum: "2", type: "number" },
+        { paynum: "3", type: "number" },
+        { paynum: "$5", type: "USD" },
+      ],
+      [
+        { paynum: "4", type: "number" },
+        { paynum: "5", type: "number" },
+        { paynum: "6", type: "number" },
+        { paynum: "$10", type: "USD" },
+      ],
+      [
+        { paynum: "7", type: "number" },
+        { paynum: "8", type: "number" },
+        { paynum: "9", type: "number" },
+        { paynum: "$20", type: "USD" },
+      ],
+      [
+        { paynum: ".", type: "number" },
+        { paynum: "0", type: "number" },
+        { paynum: "X", inconz: "th" },
+        { paynum: "$55", type: "USD" },
+      ],
+    ],
+    numFormatted: 0.0,
+  }),
 };
 </script>
